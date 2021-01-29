c5d52841bc48811cd0617f5d3966295f0cbfb12a
Andrei Pereira <andrei.pereira@lnls.br>
2021-01-29 19:18:45 -0300

    general/: Add more examples for move_and_repeat script

---
+++ general/delta_test.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# DELTA TEST SCRIPT
+
+./move_and_repeat.py \
+--points-file "delta_test.txt" \
+--repeat-count 10 \
+--repeat-reverse-at-end false \
+--cmd-pv und:EnMove-Cmd \
+--sp-pv und:AxisAMovePos-SP \
+--check-after-motion "@und:AxisAActualPosition-Mon == @und:AxisAMovePos-SP +- 0.1" \
+--before-motion-timeout 10.0 \
+--during-motion-timeout 10.0 \
+--after-motion-timeout 10.0 \
+--emergency-action-pv und:Stop-Cmd \
+--emergency-action-value True \
+--log-filename test_log
+
